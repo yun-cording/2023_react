8d875ee16703e7cd6e8b9b8e59a9348154c73a1d
ysh <zdbstjdgnsz@naver.com>
2023-08-21 13:06:21 +0900

    20230821

---
+++ src/component/Hello.js
@@ -0,0 +1,19 @@
+// javascript 에서 변수 설정 : var(변수) >>>>> let(변수) >>>>const
+//ES6 이후, 이를 보완하기 위해 추가 된 변수 선언 방식이 let 과 const이다.
+//  let : 일반적인 변수 , const: 상수
+
+import World from "./World";
+
+
+
+const Hello = function () {
+  return(
+    // jsx는 하나의 태그만 만들 수 있음
+    <>
+    <h2>Hello</h2>
+    <World></World>    
+    </>
+  )
+}
+
+export default Hello;
---
+++ src/component/Welcome.js
@@ -0,0 +1,8 @@
+const Welcome = () => { // 람다식 방식 function 생략 가능
+  return(
+    <h2>Welcome</h2>
+
+  )
+}
+
+export default Welcome;
